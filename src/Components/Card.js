48d8136a26c3a624f2dc7830f01063afc08c7898
Tochiisaac <114114023+Tochiisaac@users.noreply.github.com>
2023-04-06 17:19:03 -0600

    Made a card that will be reusable that contains image, date, name

---
+++ src/Components/Card.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+
+        <div>Image</div>
+        <div>Date</div>
+        <div>Obituary Message</div>
+        <div>Name</div>
+
+
+
+
+    </div>
+  )
+}
+
+export default card
