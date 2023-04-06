f775c7664fbd646c9c73f4fc786f85c89f6c4283
Bobinho100 <114113147+Bobinho100@users.noreply.github.com>
2023-04-06 17:05:50 -0600

    making the opacity in the layout component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import Overlay from "./Components/Overlay";
 import Layout from "./Components/Layout";
+import { useState } from "react";
 
 
 function App() {
+
+
+  const [obituaryFrom, setObituaryForm] = useState(false);
+
+  const obituaryClick = () => setObituaryForm(true);
+
+
   return(
-    <Overlay />
+    <div>
+
+      <Layout 
+      obituaryFrom = {obituaryFrom}
+      setObituaryForm = {setObituaryForm}
+      obituaryClick = {obituaryClick}
+
+      
+      
+      />
+      
+      
+      {obituaryFrom && <Overlay 
+      
+      
+      />}
+
+    </div>
+
+    
+    
   )
 
 }
--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,22 +1,29 @@
 import React from 'react'
 import "../Styles/Layout.css"
 
-const Layout = () => {
+const Layout = ({obituaryForm, setObituaryForm, obituaryClick}) => {
   return (
     <>
-        <div className='navbar'>
-            <div></div>
-            <h1>The last Show</h1>
-            <div className='navbar--obituary'>
-            <h4 > + New Obituary</h4>
-                
-            </div>
-           
-        </div>
+        <div className= "">
 
-        <div className='obituary--not-yet'>
-            <p>No Obituary Yet</p>
+            <div className= {`navbar  ${obituaryForm ? 'overlay-active': ''}`}>
+                <div></div>
+                <h1>The last Show</h1>
+                <div className='navbar--obituary'>
+                <h4 role='button' onClick={obituaryClick}> + New Obituary</h4>
+                    
+                </div>
+            
+            </div>
+
+            <div className='obituary--not-yet'>
+                <p>No Obituary Yet</p>
+            </div>
+
+
+
         </div>
+        
     
 
 
