8c4ff293fc2ec049811afc6159945923abdd96d4
Bobinho100 <114113147+Bobinho100@users.noreply.github.com>
2023-04-06 09:32:55 -0600

    Made a new component called Overlay where i have a form for the obituary inputs

--- src/Components/Overlay.js
+++ src/Components/Overlay.js
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Styles/Overlay.css';
+import ObituaryImage from '../Styles/obituary_image2.png';
 
 const Overlay = () => {
+
+
+    const [flieSelect, setFileSelect] = useState(null);
+    const [name, setName] = useState('');
+    const [born, setBorn] = useState('');
+    const [died, setDied] = useState('')
+
+    const fileChange = (event) =>{
+        setFileSelect(event.target.files[0].name);
+    }
+    const formHandler = (event) => {
+        event.preventDefault();
+        
+    }
+
+
   return (
-    <div>Overlay</div>
+    <>
+
+        <form onSubmit={formHandler}>
+            <p>Create a New Obituary</p>
+
+
+            <label htmlFor='img'className='file-link' >
+                {flieSelect ? `Select an image for the deceased (${flieSelect})`: `Select an image for the deceased`}
+                
+                </label>
+            
+            <div>
+                <input type='file' id='img'name = 'img' accept='image/*' className='hidden' onChange={fileChange}></input>
+
+            </div>
+            <div className='name--input'>
+                <input type= 'text' className='name'></input>
+
+            </div>
+
+            
+
+            <div className='date'>
+                <label htmlFor='born'>Born:</label>
+                <input id='born' name='born' type='datetime-local'></input>
+                <label htmlFor='died'>Died:</label>
+                <input id='died' name='died' type='datetime-local'></input>
+
+            </div>
+
+
+            <button>Write Obituary</button>
+
+           
+
+
+
+
+        </form>
+
+       
+    
+    
+    
+    </>
   )
 }
 
